88efd013c1e2b5c4808eeca473a020f254842509
Nasian <87439662+nasiana@users.noreply.github.com>
2021-11-15 21:01:46 +0000

    Add files via upload

---
+++ Live_Lecture.py
@@ -0,0 +1,33 @@
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+class Bus(Vehicle):
+    def seating_capacity(self, capacity = 50):
+        pass
+
+# ex1 = Bus("bus", 15, 60)
+# print(super.ex1.seating_capacity)
+
+class Cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def info(self):
+        print(f"I am a cat. My name is {self.name}. I am {self.age} years old.")
+    def make_sound(self):
+        print("Meow")
+
+class Dog(Cat):
+    def info(self):
+        print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
+    def make_sound(self):
+        print("Bark")
+
+dog = Dog("goldie", 5)
+(dog.info())
+print(dog.make_sound())
